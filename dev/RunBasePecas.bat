5fb99a21eaca212d5bd51fceb095abf990d6874b
Ber468 <bernardoradin.dalpozzo@gmail.com>
2022-08-03 20:08:40 -0300

    Commit de 03/08/2022-20h08m40s

---
+++ dev/RunBasePecas.bat
@@ -0,0 +1,7 @@
+rem mongod --port 27018 --dbpath C:\Users\berna\Documents\ProjectsVsCode\BRP-Pecas\mongo\basePecas --auth
+rem mongod --port 27018 --dbpath C:\Users\182715\Documents\BRP-Pecas\mongo\basePecas --auth
+    mongod --port 27018 --dbpath C:\Users\Bernardo\Documents\ProjectsVsCode\BRP-Pecas\mongo\basePecas --auth
+
+ rem mongo --port 27018  -- sem autenticação
+ rem mongo -u "admin" -p "admin"  --authenticationDatabase "basePecas" --port 27018 
+ rem compass e node:  mongodb://admin:admin@localhost:27018/basePecas?authSource=basePecas
